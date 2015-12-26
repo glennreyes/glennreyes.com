163cc3d7c44ebbf78e237c7b2afa9d2d2ce281f5
Glenn Reyes <office@glennreyes.com>
2015-12-26 23:21:50 +0100

    Gulpfile fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 import Toggle from './toggle'
 
-// const nav = new Navigation('.nav-toggle')
 const navToggle = new Toggle('.nav-toggle', {
   modifier: 'nav-open',
   name: 'nav'
--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -1,76 +1,64 @@
 import Util from './util'
-import jQuery from 'jquery'
-
-const Toggle = (($) => {
+import $ from 'jquery'
 
 
-  /**
-   * Constants
-   */
+/**
+ * Constants
+ */
 
-  const NAME = 'toggle'
-  const VERSION = '1.0.0'
-  const DATA_KEY = 'gr.toggle'
-  const EVENT_KEY = `.${DATA_KEY}`
+const NAME = 'toggle'
+const VERSION = '1.0.0'
+const DATA_KEY = 'gr.toggle'
+const EVENT_KEY = `.${DATA_KEY}`
 
-  const Default = {
-    modifier: 'active',
-    target: document.body
+const Default = {
+  modifier: 'active',
+  target: document.body
+}
+const DefaultType = {
+  modifier: 'string',
+  target: '(string|element)'
+}
+const Event = {
+  CLICK: `click${EVENT_KEY}`
+}
+
+
+/**
+ * Class Definition
+ */
+
+class Toggle {
+
+  constructor(element, config) {
+    this._element = element
+    this._config  = this._getConfig(config)
+
+    this._addEventListener()
   }
 
-  const DefaultType = {
-    modifier: 'string',
-    target: '(string|element)'
-  }
 
-  const Event = {
-    CLICK: `click${EVENT_KEY}`
-  }
+  // public
 
-
-  /**
-   * Class Definition
-   */
-
-  class Toggle {
-
-    constructor(element, config) {
-      this._element = element
-      this._config  = this._getConfig(config)
-
-      this._addEventListener()
-    }
-
-
-    // public
-
-    toggle() {
-      $(this._config.target).toggleClass(this._config.modifier)
-    }
-
-
-    // private
-
-    _getConfig(config) {
-      config = $.extend({}, Default, config)
-      Util.typeCheckConfig(NAME, config, DefaultType)
-      return config
-    }
-
-    _addEventListener() {
-      $(this._element).on(Event.CLICK, () => {
-        this.toggle()
-      })
-    }
+  toggle() {
+    $(this._config.target).toggleClass(this._config.modifier)
   }
 
 
   // private
 
-  //   // return config
+  _getConfig(config) {
+    config = $.extend({}, Default, config)
+    Util.typeCheckConfig(NAME, config, DefaultType)
+    return config
+  }
 
-  return Toggle
+  _addEventListener() {
+    $(this._element).on(Event.CLICK, () => {
+      this.toggle()
+    })
+  }
+}
 
-})(jQuery)
 
 export default Toggle
