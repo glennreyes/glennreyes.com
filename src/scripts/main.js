d24c4f14f4477dadf262bfa50654c5084c87b908
Glenn Reyes <glenn@glennreyes.com>
2016-11-15 15:36:15 +0100

    Remove unused JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,6 @@ import StickyHeader from './sticky-header'
 import Tabs from './tabs'
 
 
-
-
-
 /**
  * Navigation Toggle
  */
@@ -18,32 +15,11 @@ const navToggle = new Toggle('.nav-toggle, .header-nav__item', {
   modifier: 'nav-open'
 })
 
-
-
-
-
-/**
- * Footer Links
- */
-const footerLinks = new Tabs('.tab-link', {
-  close: '.close',
-  content: '.footer-section',
-  modifier: 'footer-section--open'
-})
-
-
-
-
-
 /**
  * Anchor Scroll
  */
 const anchorScroll =
   new AnchorScroll('.logo, .header-nav__item, .scroll-down, .scroll-up, .tab-link')
-
-
-
-
 
 /**
  * Sticky Header
