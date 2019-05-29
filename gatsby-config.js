fab8d115bbc43d3f4c40ddedf9cf68180dd8aad3
Glenn Reyes <glenn@glennreyes.com>
2019-05-29 11:47:29 +0200

    Config manifest and site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Glenn Reyes`,
+    description: `I help people create beautiful products through web technologies.`,
+    author: `@glnnrys`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -14,11 +14,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Glenn Reyes`,
+        short_name: `Glenn R.`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
