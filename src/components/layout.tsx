66cfe9a693eedf6a342cb3fd270499bace55990f
Glenn Reyes <glenn@glennreyes.com>
2019-05-29 13:39:56 +0200

    Destructure title

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -26,26 +26,30 @@ const Layout = ({ children }: LayoutProps) => (
         }
       }
     `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
+    render={data => {
+      const { title } = data.site.siteMetadata;
+
+      return (
+        <>
+          <Header siteTitle={title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </>
+      );
+    }}
   />
 );
 
