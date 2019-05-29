80de28608e4be6808fe796e9c28498528fa429ce
Glenn Reyes <glenn@glennreyes.com>
2019-05-29 11:49:25 +0200

    Replace icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,
