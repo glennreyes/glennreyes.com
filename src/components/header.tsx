84a4ac91ce571cde87f437584ddbb39c43093a4e
Glenn Reyes <glenn@glennreyes.com>
2019-05-29 17:04:26 +0200

    Move title query to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,46 @@
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react';
 
-type HeaderProps = typeof defaultProps;
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  const { title } = data.site.siteMetadata;
 
-const defaultProps = {
-  siteTitle: '',
-};
-
-const Header = ({ siteTitle }: HeaderProps) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
-
-Header.defaultProps = defaultProps;
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {title}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,44 +18,27 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => {
-      const { title } = data.site.siteMetadata;
-
-      return (
-        <ThemeProvider>
-          <>
-            <GlobalStyle />
-            <Header siteTitle={title} />
-            <div
-              style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `0px 1.0875rem 1.45rem`,
-                paddingTop: 0,
-              }}
-            >
-              <main>{children}</main>
-              <footer>
-                © {new Date().getFullYear()}, Built with
-                {` `}
-                <a href="https://www.gatsbyjs.org">Gatsby</a>
-              </footer>
-            </div>
-          </>
-        </ThemeProvider>
-      );
-    }}
-  />
+  <ThemeProvider>
+    <>
+      <GlobalStyle />
+      <Header />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  </ThemeProvider>
 );
 
 export default Layout;
