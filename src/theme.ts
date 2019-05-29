2b7bb6d36388263a3c140b0efef0e2a465549438
Glenn Reyes <glenn@glennreyes.com>
2019-05-29 13:58:22 +0200

    Add initial theme

---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+const theme = {
+  borders: [0, 1, 2, 4],
+  breakpoints: ['768px', '1024px'],
+  colors: {
+    black: '#000',
+    gray: {
+      0: '#e5e5e5',
+      1: '#c4c4c4',
+      2: '#767676',
+      3: '#2e2e2e',
+    },
+    white: '#fff',
+  },
+  fonts: {
+    sans: 'system-ui, sans-serif',
+    mono: 'Menlo, monospace',
+  },
+  fontSizes: [16, 20, 24, 32, 48, 72],
+  fontWeights: ['normal', 'bold'],
+  lineHeights: [1, 1.25, 1.5],
+  radii: [0, 8, 16],
+  space: [0, 4, 8, 16, 32, 64, 128],
+};
+
+export default theme;
