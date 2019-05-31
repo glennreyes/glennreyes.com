9844e9f7cf286b9c9acf0035a674555d266150cf
Glenn Reyes <glenn@glennreyes.com>
2019-05-31 15:29:38 +0200

    Center button content

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+  align-items: center;
   background: none;
   border: none;
   cursor: pointer;
   display: flex;
   font-family: ${p => p.theme.fonts.sans};
+  justify-content: center;
   padding: 0;
 
   &:focus {
