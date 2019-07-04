07a274369f49d5df1937e665fc5710a18895bee1
Glenn Reyes <glenn@glennreyes.com>
2019-07-04 10:43:39 +0200

    Add sequenced transition via keyframes
    Basically implemented keyframes into the existing animation.
    https://popmotion.io/pose/api/react-config/#config-options-poses-transition

--- src/components/menu-button.tsx
+++ src/components/menu-button.tsx
@@ -1,8 +1,10 @@
+import ms from 'ms';
 import React from 'react';
 import posed from 'react-pose';
 import styled from 'styled-components';
 import Button from './button';
 import MenuToggleContext from './menu-toggle-context';
+import { system } from '../theme';
 
 const Wrapper = styled(Button)`
   flex-direction: column;
@@ -19,14 +21,62 @@ const Stroke = styled.span`
   width: 20px;
 `;
 
+const transitionDefaultProps = {
+  duration: ms(system.transition),
+  type: 'keyframes',
+};
+
+interface TransitionProps {
+  from: string;
+  to: string;
+}
+
+const closeTransition = ({ from, to }: TransitionProps) => ({
+  rotate: {
+    ...transitionDefaultProps,
+    values: [from, to, to],
+  },
+  y: {
+    ...transitionDefaultProps,
+    values: [from, from, to],
+  },
+});
+
+const openTransition = ({ from, to }: TransitionProps) => ({
+  rotate: {
+    ...transitionDefaultProps,
+    values: [from, from, to],
+  },
+  y: {
+    ...transitionDefaultProps,
+    values: [from, to, to],
+  },
+});
+
 const TopStroke = posed(Stroke)({
-  closed: { rotate: '0deg', y: 0 },
-  open: { rotate: '135deg', y: 4 },
+  closed: {
+    rotate: '0deg',
+    transition: closeTransition,
+    y: 0,
+  },
+  open: {
+    rotate: '135deg',
+    transition: openTransition,
+    y: 4,
+  },
 });
 
 const BottomStroke = posed(Stroke)({
-  closed: { rotate: '0deg', y: 0 },
-  open: { rotate: '-135deg', y: -4 },
+  closed: {
+    rotate: '0deg',
+    transition: closeTransition,
+    y: 0,
+  },
+  open: {
+    rotate: '-135deg',
+    transition: openTransition,
+    y: -4,
+  },
 });
 
 const MenuButton = () => {
