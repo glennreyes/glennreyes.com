8596d7b150df500048b4333ff7927ab3d4d70772
Glenn Reyes <glenn@glennreyes.com>
2019-07-08 11:02:08 +0200

    Add subtle animation

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,7 +1,10 @@
+import ms from 'ms';
 import React from 'react';
+import posed from 'react-pose';
 import styled from 'styled-components';
 import Link from './link';
 import MenuToggleContext from './menu-toggle-context';
+import { system } from '../theme';
 
 const links = [
   {
@@ -26,7 +29,7 @@ const links = [
   },
 ];
 
-const Wrapper = styled.nav`
+const Nav = styled.nav`
   background: ${p => p.theme.headerBg};
   display: flex;
   flex-direction: column;
@@ -38,15 +41,22 @@ const Wrapper = styled.nav`
   width: 100%;
 
   ${p => p.theme.media.desktop`
-    background: none;
-    flex-direction: row;
-    height: auto;
-    left: auto;
-    position: static;
-    top: auto;
-    width: auto;
-  `}
+  background: none;
+  flex-direction: row;
+  height: auto;
+  left: auto;
+  position: static;
+  top: auto;
+  width: auto;
+`}
 `;
+
+const Wrapper = posed(Nav)({
+  close: { height: 0 },
+  open: {
+    height: `calc(100vh - ${system.space[5]}px)`,
+  },
+});
 
 const MenuLink = styled(Link)`
   border-color: ${p => p.theme.bg};
@@ -60,19 +70,25 @@ const MenuLink = styled(Link)`
     border-top: none;
   }
 
+  ${p => p.theme.media.tablet`
+    padding-left: ${p.theme.space[4]}px;
+    padding-right: ${p.theme.space[4]}px;
+  `}
+
   ${p => p.theme.media.desktop`
     border: none;
     margin: 0 ${p.theme.space[2]}px;
+    padding-left: ${p.theme.space[3]}px;
+    padding-right: ${p.theme.space[3]}px;
   `}
 `;
 
 const Menu = () => {
   const { isOpen, close } = React.useContext(MenuToggleContext);
-
-  if (!isOpen) return null;
+  const pose = isOpen ? 'open' : 'close';
 
   return (
-    <Wrapper>
+    <Wrapper duration={ms(system.transition)} pose={pose}>
       {links.map(({ label, path }) => (
         <MenuLink key={label.toLowerCase()} onClick={() => close()} to={path}>
           {label}
