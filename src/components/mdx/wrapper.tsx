8c91528b1f5d658fc00032d07774cecd639b39d0
Glenn Reyes <glenn@glennreyes.com>
2019-08-03 00:11:26 +0200

    Keep commenting, right

--- src/components/mdx/wrapper.tsx
+++ src/components/mdx/wrapper.tsx
@@ -7,6 +7,8 @@ type WrapperProps = {
 
 const Wrapper = ({ children }: WrapperProps) => {
   React.useEffect(() => {
+    // Always reset the slugger on every re-render, otherwise occurence indizes
+    // get incremented on every page transition.
     slugger.reset();
   });
 
