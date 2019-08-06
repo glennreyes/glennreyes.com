63b133470a9690d73ac1c255cb427acd05a23228
Glenn Reyes <glenn@glennreyes.com>
2019-08-06 20:09:39 +0200

    Fix intro spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,14 @@ const Section = styled.section`
 `;
 
 const IntroSection = styled(Section)`
+  margin-bottom: ${p => p.theme.space[8]}px;
+  margin-top: ${p => p.theme.space[8]}px;
+
+  ${p => p.theme.media.tablet`
+    margin-bottom: ${p.theme.space[8] + p.theme.space[7]}px;
+    margin-top: ${p.theme.space[8] + p.theme.space[7]}px;
+  `}
+
   ${p => p.theme.media.desktop`
     margin-bottom: ${p.theme.space[9]}px;
     margin-top: ${p.theme.space[9]}px;
