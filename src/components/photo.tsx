c75ce6f9cfe5d2c79923ddf7142677d8a40c45d3
Glenn Reyes <glenn@glennreyes.com>
2019-08-08 22:33:49 +0200

    Remove obsolete instructions

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -4,17 +4,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { PhotoQuery } from '../types/generated/graphql';
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `StaticQuery`: https://gatsby.dev/staticquery
- */
-
 const StyledImg = styled(Img)`
   border-radius: 50%;
   width: 128px;
