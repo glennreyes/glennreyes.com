1fa9baecc72f8cf316e6c0cc180a2eea5e58c576
Glenn Reyes <glenn@glennreyes.com>
2019-08-12 18:06:40 +0200

    Use single quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: [`gatsby-remark-images`],
+        plugins: ['gatsby-remark-images'],
       },
     },
     'gatsby-transformer-sharp',
