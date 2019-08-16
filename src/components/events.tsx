d4aca5161edb9ab910d30e1827e4282306ae307a
Glenn Reyes <glenn@glennreyes.com>
2019-08-16 22:16:59 +0200

    Split upcoming & past events

---
+++ src/components/events.tsx
@@ -0,0 +1,124 @@
+import dayjs from 'dayjs';
+import React from 'react';
+import styled from 'styled-components';
+
+const LineHeading = styled.h3`
+  align-items: baseline;
+  color: ${p => p.theme.colors.textSecondary};
+  display: flex;
+  font-size: ${p => p.theme.fontSizes[0]}px;
+  font-weight: ${p => p.theme.fontWeights.normal};
+  line-height: ${p => p.theme.lineHeights.heading};
+  margin: ${p => p.theme.space[4]}px 0 0;
+  text-transform: uppercase;
+  white-space: nowrap;
+
+  &::after {
+    background: ${p => p.theme.colors.border};
+    content: '';
+    display: block;
+    height: 4px;
+    margin-left: ${p => p.theme.space[1]}px;
+    width: 100%;
+  }
+`;
+
+const Wrapper = styled.ul`
+  list-style: none;
+  margin: ${p => p.theme.space[2]}px 0 0;
+  padding: 0;
+`;
+
+const Event = styled.li`
+  color: ${p => p.theme.colors.textSecondary};
+  font-size: ${p => p.theme.fontSizes[1]}px;
+`;
+
+type EventsProps = {
+  events?: {
+    date?: string;
+    dateFormatted?: string;
+    id: string;
+    location?: {
+      city?: string;
+      country: string;
+    };
+    startDate?: string;
+    startDateFormatted?: string;
+    title?: string;
+  }[];
+};
+
+const Events = ({ events }: EventsProps) => {
+  const upcomingEvents = Array.isArray(events)
+    ? events.filter(event =>
+        dayjs(event.date || event.startDate).isAfter(dayjs()),
+      )
+    : [];
+  const pastEvents = Array.isArray(events)
+    ? events.filter(event =>
+        dayjs(event.date || event.startDate).isBefore(dayjs()),
+      )
+    : [];
+
+  return (
+    <>
+      {upcomingEvents.length > 0 && (
+        <>
+          <LineHeading>Upcoming Events</LineHeading>
+          <Wrapper>
+            {upcomingEvents.map(event => (
+              <Event key={event.id}>
+                {(event.date || event.startDate) &&
+                  (event.dateFormatted || event.startDateFormatted) && (
+                    <time dateTime={event.date || event.startDate}>
+                      {event.dateFormatted || event.startDateFormatted}
+                    </time>
+                  )}
+                {event.title && (
+                  <>
+                    {' · '}
+                    <strong>{event.title}</strong>
+                  </>
+                )}
+                {event.location &&
+                  event.location.city &&
+                  event.location.country &&
+                  ` · ${event.location.city}, ${event.location.country}`}
+              </Event>
+            ))}
+          </Wrapper>
+        </>
+      )}
+      {pastEvents.length > 0 && (
+        <>
+          <LineHeading>Past Events</LineHeading>
+          <Wrapper>
+            {pastEvents.map(event => (
+              <Event key={event.id}>
+                {(event.date || event.startDate) &&
+                  (event.dateFormatted || event.startDateFormatted) && (
+                    <time dateTime={event.date || event.startDate}>
+                      {event.dateFormatted || event.startDateFormatted}
+                    </time>
+                  )}
+                {event.title && (
+                  <>
+                    {' · '}
+                    <strong>{event.title}</strong>
+                  </>
+                )}
+                {event.location &&
+                  event.location.city &&
+                  event.location.country &&
+                  ` · ${event.location.city}, ${event.location.country}`}
+              </Event>
+            ))}
+          </Wrapper>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Events;
--- src/components/talks/talks.tsx
+++ src/components/talks/talks.tsx
@@ -1,45 +1,13 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
-import styled from 'styled-components';
 import Card from '../card';
 import Cards from '../cards';
 import CardBody from '../card-body';
 import CardLink from '../card-link';
 import CardTitle from '../card-title';
+import Events from '../events';
 import { TalksQuery } from '../../types/generated/graphql';
-
-const LineHeading = styled.h3`
-  align-items: baseline;
-  color: ${p => p.theme.colors.textSecondary};
-  display: flex;
-  font-size: ${p => p.theme.fontSizes[0]}px;
-  font-weight: ${p => p.theme.fontWeights.normal};
-  line-height: ${p => p.theme.lineHeights.heading};
-  margin: ${p => p.theme.space[4]}px 0 0;
-  text-transform: uppercase;
-  white-space: nowrap;
-
-  &::after {
-    background: ${p => p.theme.colors.border};
-    content: '';
-    display: block;
-    height: 4px;
-    margin-left: ${p => p.theme.space[1]}px;
-    width: 100%;
-  }
-`;
-
-const Events = styled.ul`
-  list-style: none;
-  margin: ${p => p.theme.space[2]}px 0 0;
-  padding: 0;
-`;
-
-const Event = styled.li`
-  color: ${p => p.theme.colors.textSecondary};
-  font-size: ${p => p.theme.fontSizes[1]}px;
-`;
 
 const Talks = () => {
   const data: TalksQuery = useStaticQuery(
@@ -88,30 +56,7 @@ const Talks = () => {
                   <MDXRenderer>{body}</MDXRenderer>
                 </CardBody>
               )}
-              <LineHeading>Events</LineHeading>
-              <Events>
-                {Array.isArray(events) &&
-                  events.map(event => (
-                    <Event key={event.id}>
-                      {(event.date || event.startDate) &&
-                        (event.dateFormatted || event.startDateFormatted) && (
-                          <time dateTime={event.date || event.startDate}>
-                            {event.dateFormatted || event.startDateFormatted}
-                          </time>
-                        )}
-                      {event.title && (
-                        <>
-                          {' · '}
-                          <strong>{event.title}</strong>
-                        </>
-                      )}
-                      {event.location &&
-                        event.location.city &&
-                        event.location.country &&
-                        ` · ${event.location.city}, ${event.location.country}`}
-                    </Event>
-                  ))}
-              </Events>
+              <Events events={events} />
             </Card>
           ),
       )}
