bbb7595fac037edadc12ebb227f1d471004ad16a
Glenn Reyes <glenn@glennreyes.com>
2022-11-13 01:49:20 +0100

    Use head file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import appleTouchIcon from '../assets/apple-touch-icon.png';
-import favicon from '../assets/favicon.png';
 import './main.css';
 import { Plus_Jakarta_Sans } from '@next/font/google';
 import type { ReactNode } from 'react';
@@ -19,20 +17,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html className={plusJakartaSans.variable} lang="en">
-      <head>
-        <title>
-          Glenn Reyes - Software engineer, tech speaker and workshop instructor
-        </title>
-        <meta
-          content="Software engineer, tech speaker and workshop instructor with a passion for building innovative products and user interfaces using cutting edge web technologies."
-          name="description"
-        />
-        <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href={favicon.src} rel="icon" type="image/png" />
-        <link href={appleTouchIcon.src} rel="apple-touch-icon" />
-        <link href={appleTouchIcon.src} rel="apple-touch-icon" />
-        <link href="/site.webmanifest" rel="manifest" />
-      </head>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
       <body>
         <Navbar />
         <main>{children}</main>
