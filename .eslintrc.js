8930ed370d2cd928bf60cd110ee8b789bf670c22
Glenn Reyes <glenn@glennreyes.com>
2022-12-06 20:17:55 +0100

    Adjust linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
+/** @type {import('eslint').Linter.BaseConfig} */
+
 module.exports = {
   extends: ['next/core-web-vitals', 'banana/react'],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
+  parserOptions: { project: './tsconfig.json' },
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
   },
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config} */
 
 module.exports = {
+  importOrder: ['^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  pluginSearchDirs: false,
   plugins: [require('@trivago/prettier-plugin-sort-imports'), require('prettier-plugin-tailwindcss')],
   printWidth: 120,
   singleQuote: true,
