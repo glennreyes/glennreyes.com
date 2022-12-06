7b40c6bff61843cf528605694bc2e0d71a503de0
Glenn Reyes <glenn@glennreyes.com>
2022-12-06 12:35:16 +0100

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'banana/react'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+};
