fd591c740f89ca49dee62a1960cd840259a2daea
Glenn Reyes <glenn@glennreyes.com>
2023-03-11 08:50:34 +0100

    Simplify Navbar component

--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -1,14 +1,12 @@
-import type { ComponentProps } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
 
-type NavbarProps = Omit<ComponentProps<'nav'>, 'className'>;
+type NavbarProps = Omit<ComponentPropsWithoutRef<'nav'>, 'className'>;
 
-export function Navbar({ children, ...props }: NavbarProps) {
+export function Navbar(props: NavbarProps) {
   return (
     <header className="sticky top-0 bg-white/75 supports-[backdrop-filter]:backdrop-blur-md">
       <div className="p-4 md:px-8">
-        <nav className="mx-auto flex max-w-5xl justify-end gap-2 md:justify-center md:gap-4" {...props}>
-          {children}
-        </nav>
+        <nav className="mx-auto flex max-w-5xl justify-end gap-2 md:justify-center md:gap-4" {...props} />
       </div>
     </header>
   );
