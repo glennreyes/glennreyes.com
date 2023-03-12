2dd2c95f8c1a01606097f41ca65bad91f8110d33
Glenn Reyes <glenn@glennreyes.com>
2023-03-12 08:23:18 +0100

    Remove background color

--- components/ui/layout/Body.tsx
+++ components/ui/layout/Body.tsx
@@ -3,5 +3,5 @@ import type { ComponentPropsWithoutRef } from 'react';
 type BodyProps = Omit<ComponentPropsWithoutRef<'body'>, 'className'>;
 
 export function Body(props: BodyProps) {
-  return <body className="min-h-screen min-w-[320px] bg-gray-50" {...props} />;
+  return <body className="min-h-screen min-w-[320px]" {...props} />;
 }
