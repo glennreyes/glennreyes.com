385b70dfb0e24671fdea8f410274af18e613a9c6
Glenn Reyes <glenn@glennreyes.com>
2023-03-16 10:16:28 +0100

    Use grid for posts on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-      {/* @ts-ignore-error Server Components */}
-      <PostsSection />
+      <section className="grid grid-cols-12">
+        <div className="col-span-full lg:col-span-7 xl:col-span-8">
+          {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+          {/* @ts-ignore-error Server Components */}
+          <PostsSection />
+        </div>
+      </section>
     </>
   );
 }
