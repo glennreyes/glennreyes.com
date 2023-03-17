d205086cc89f3774e99fcda1329372a67d923afa
Glenn Reyes <glenn@glennreyes.com>
2023-03-17 15:36:49 +0100

    Add accents to post

--- components/ui/home/Post.tsx
+++ components/ui/home/Post.tsx
@@ -19,18 +19,18 @@ export function Post({ post }: PostProps) {
 
   return (
     <article className="group relative grid gap-3 py-6">
-      <div className="relative z-10 px-4">
+      <div className="relative z-10 px-4 text-gray-400">
         <time dateTime={publishedAtValue}>{publishedAt}</time> ·{' '}
         <time dateTime={readingTimeValue}>{post.readingTime.text}</time>
       </div>
       <h3 className="px-4 text-lg font-semibold tracking-tight">
         <Link href={`/posts/${post.slug}`}>
-          <span className="absolute inset-0 z-20 md:-inset-2" />
+          <span className="md:-inset-z-2 absolute inset-0 z-20" />
           <span className="relative z-10">{post.title}</span>
         </Link>
       </h3>
-      <p className="relative z-10 px-4">{post.excerpt}</p>
-      <div className="absolute inset-0 scale-95 bg-stone-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:rounded-2xl md:-inset-2" />
+      <p className="relative z-10 px-4 text-stone-500">{post.excerpt}</p>
+      <div className="absolute inset-0 scale-95 bg-stone-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:rounded-2xl md:-inset-x-2" />
     </article>
   );
 }
