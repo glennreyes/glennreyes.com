5348a2143fc282b30a2c4c63eb34b0b3a41923ac
Glenn Reyes <glenn@glennreyes.com>
2023-03-21 14:11:35 +0100

    Delete unused pages

--- app/journal/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function JournalPage() {
-  return (
-    <div>
-      <h1>Journal</h1>
-      <h2>My thoughts directly copied from my brain.</h2>
-    </div>
-  );
-}
--- app/workshops/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function WorkshopsPage() {
-  return (
-    <div>
-      <h1>Workshops</h1>
-      <h2>Teaching web technologies to developers.</h2>
-    </div>
-  );
-}
