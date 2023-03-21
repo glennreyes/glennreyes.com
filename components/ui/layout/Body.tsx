0dc3f4c7d8a97283b02b2914d480649f8aee60dd
Glenn Reyes <glenn@glennreyes.com>
2023-03-21 09:57:19 +0100

    Fix layout spacing

--- components/ui/layout/Body.tsx
+++ components/ui/layout/Body.tsx
@@ -3,5 +3,5 @@ import type { ComponentPropsWithoutRef } from 'react';
 type BodyProps = Omit<ComponentPropsWithoutRef<'body'>, 'className'>;
 
 export function Body(props: BodyProps) {
-  return <body className="flex min-h-screen min-w-[320px] flex-col gap-12 antialiased" {...props} />;
+  return <body className="min-h-screen min-w-[320px] antialiased" {...props} />;
 }
