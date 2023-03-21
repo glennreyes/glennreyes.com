473e98cea1da3cd0ecca7f18be12c88492ed043f
Glenn Reyes <glenn@glennreyes.com>
2023-03-21 11:15:03 +0100

    Remove unused params from layout

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -2,13 +2,8 @@ import type { ReactNode } from 'react';
 import { Article } from '~/components/ui/layout/Article';
 import { ArticleBody } from '~/components/ui/layout/ArticleBody';
 
-interface LayoutParams {
-  slug: string;
-}
-
 interface LayoutProps {
   children: ReactNode;
-  params: LayoutParams;
 }
 
 export default function Layout({ children }: LayoutProps) {
