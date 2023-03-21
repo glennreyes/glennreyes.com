e6d4e86d03aedd1fda2f66fcad8136bd322299af
Glenn Reyes <glenn@glennreyes.com>
2023-03-21 12:56:03 +0100

    Adjust spacing between elements

--- components/ui/layout/Main.tsx
+++ components/ui/layout/Main.tsx
@@ -3,5 +3,5 @@ import type { ComponentPropsWithoutRef } from 'react';
 export type MainProps = Omit<ComponentPropsWithoutRef<'main'>, 'className'>;
 
 export function Main(props: MainProps) {
-  return <main className="space-y-12" {...props} />;
+  return <main className="space-y-16" {...props} />;
 }
