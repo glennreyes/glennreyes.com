8a9770ec302b49fb5f4aad2c447c53c4fd869bb9
Glenn Reyes <glenn@glennreyes.com>
2023-03-22 15:26:16 +0100

    Handle mobile layout for feed

--- components/ui/feed/Feed.tsx
+++ components/ui/feed/Feed.tsx
@@ -7,11 +7,11 @@ interface FeedProps extends Omit<ComponentPropsWithoutRef<'div'>, 'className'> {
 export function Feed({ children, title, ...props }: FeedProps) {
   if (title) {
     return (
-      <div className="grid grid-cols-4" {...props}>
-        <div className="border-l border-stone-100 px-8">
-          <h2 className="sticky top-16 font-semibold text-emerald-700/90">{title}</h2>
+      <div className="grid gap-y-8 md:grid-cols-4" {...props}>
+        <div className="md:border-l md:border-stone-100 md:px-8">
+          <h2 className="font-semibold text-emerald-700/90 md:sticky md:top-20">{title}</h2>
         </div>
-        <div className="col-span-3 grid gap-8 md:gap-12">{children}</div>
+        <div className="grid gap-8 md:col-span-3 md:gap-12 lg:col-span-2">{children}</div>
       </div>
     );
   }
