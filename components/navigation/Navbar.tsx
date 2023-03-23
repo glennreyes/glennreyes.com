f64dc443bb3b184abd20ebeddd4babbade8b2cec
Glenn Reyes <glenn@glennreyes.com>
2023-03-23 11:33:28 +0100

    Add fixes to the navbar

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,4 +1,4 @@
-import { SunIcon } from '@heroicons/react/20/solid';
+import { SunIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 import type { LinkProps } from 'next/link';
 import Link from 'next/link';
@@ -14,14 +14,14 @@ export function Navbar(props: NavbarProps) {
     <header className="sticky top-0 z-20 bg-white/75 supports-[backdrop-filter]:backdrop-blur-md">
       <Container>
         <div className="flex items-center gap-2 md:gap-4">
-          <div className="flex-1">
-            <Link href="/">
+          <div className="flex flex-1">
+            <Link className="flex-none" href="/">
               <Image alt={name} className="h-10 w-10 rounded-full border-2 border-stone-200" src={photo} />
             </Link>
           </div>
-          <nav className="flex flex-1 justify-end gap-2 py-4 md:justify-center md:gap-4" {...props} />
-          <div className="flex justify-end md:flex-1">
-            <SunIcon className="h-5 w-5 text-stone-400" />
+          <nav className="flex flex-1 gap-2 py-4 md:justify-center md:gap-4" {...props} />
+          <div className="flex flex-1 justify-end">
+            <SunIcon className="h-6 w-6 text-stone-400" />
           </div>
         </div>
       </Container>
