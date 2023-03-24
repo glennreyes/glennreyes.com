a4dd60792888d055593dbed424fbe781f5151f06
Glenn Reyes <glenn@glennreyes.com>
2023-03-24 18:27:52 +0100

    Turn off next/no-html-link-for-pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,7 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'banana/react'],
   parserOptions: { project: './tsconfig.json' },
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+  },
 };
