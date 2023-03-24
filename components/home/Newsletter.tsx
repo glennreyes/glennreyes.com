d5748d55effa2a5e39af785ab332e35c808e4d18
Glenn Reyes <glenn@glennreyes.com>
2023-03-24 15:13:45 +0100

    Move newsletter form content to `NewsletterForm` component

--- components/home/Newsletter.tsx
+++ components/home/Newsletter.tsx
@@ -1,5 +1,3 @@
-import { Button } from '../ui/forms/Button';
-import { Input } from '../ui/forms/Input';
 import { Card } from '../ui/layout/Card';
 import { H4 } from '../ui/typography/H4';
 import { NewsletterForm } from './NewsletterForm';
@@ -11,26 +9,11 @@ export function Newsletter() {
         <Card.Body>
           <H4 as="h2">Stay in the loop</H4>
           <p className="text-sm text-stone-500">
-            Get subscribed for news and updates. No spam, unsubscribe at any time.
+            Get subscribed for latest news and updates. No spam, unsubscribe at any time.
           </p>
         </Card.Body>
         <Card.Body>
-          <NewsletterForm>
-            <div className="sm:flex-1">
-              <Input
-                aria-label="Email address"
-                className="peer relative z-10 w-full sm:border-none sm:focus:ring-0"
-                name="email"
-                placeholder="Your email address"
-                required
-                type="email"
-              />
-              <span className="absolute inset-0 hidden rounded-[1.25rem] border border-stone-300 p-1 transition peer-focus:border-stone-400 peer-focus:ring-4 peer-focus:ring-teal-100 sm:block" />
-            </div>
-            <div className="relative grid">
-              <Button>Subscribe</Button>
-            </div>
-          </NewsletterForm>
+          <NewsletterForm />
         </Card.Body>
       </div>
     </Card>
--- components/home/NewsletterForm.tsx
+++ components/home/NewsletterForm.tsx
@@ -1,26 +1,57 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import type { ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef, FormEventHandler } from 'react';
+import { useState, useCallback, useTransition } from 'react';
+import { Button } from '../ui/forms/Button';
+import { Input } from '../ui/forms/Input';
 
-type NewsletterFormProps = Omit<ComponentPropsWithoutRef<'form'>, 'className'>;
+type NewsletterFormProps = Omit<ComponentPropsWithoutRef<'form'>, 'children' | 'className'>;
 
 export function NewsletterForm(props: NewsletterFormProps) {
-  const router = useRouter();
+  const { push } = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isMutating = isFetching || isPending;
+  const handleSubmit = useCallback<FormEventHandler<HTMLFormElement>>(
+    async (event) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+
+      try {
+        setIsFetching(true);
+        await fetch('/subscribe', {
+          body: data,
+          headers: { 'Content-Type': 'application/json' },
+          method: 'post',
+        });
+        setIsFetching(false);
+        startTransition(() => push('/subscribe/pending'));
+      } catch {
+        setIsFetching(false);
+        // TODO: Show error message
+      }
+    },
+    [push],
+  );
 
   return (
-    <form
-      className="grid gap-y-4 gap-x-2 sm:relative sm:flex sm:p-1"
-      onSubmit={(event) => {
-        const data = new FormData(event.currentTarget);
-        const email = data.get('email');
-
-        // TODO: Subscribe email
-        console.info(email);
-
-        router.push('/thank-you');
-      }}
-      {...props}
-    />
+    <form className="grid gap-y-4 gap-x-2 sm:relative sm:flex sm:p-1" onSubmit={handleSubmit} {...props}>
+      <div className="sm:flex-1">
+        <Input
+          aria-label="Email address"
+          className="peer relative z-10 w-full sm:border-none sm:focus:ring-0"
+          disabled={isMutating}
+          name="email"
+          placeholder="Your email address"
+          required
+          type="email"
+        />
+        <span className="absolute inset-0 hidden rounded-[1.25rem] border border-stone-300 p-1 transition peer-focus:border-stone-400 peer-focus:ring-4 peer-focus:ring-teal-100 sm:block" />
+      </div>
+      <div className="relative grid">
+        <Button disabled={isMutating}>Subscribe</Button>
+      </div>
+    </form>
   );
 }
