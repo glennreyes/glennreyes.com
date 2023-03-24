08576cafdd8535feb7a8390d930d94e34be41479
Glenn Reyes <glenn@glennreyes.com>
2023-03-24 15:15:18 +0100

    Push missing helpers for newsletter subscription

---
+++ lib/newsletter.ts
@@ -0,0 +1,10 @@
+const formId = 4917153;
+const publicApiKey = 'lQbg_unCgg3LrJVBjnboyA';
+
+export function subscribe(email: string) {
+  return fetch(`https://api.convertkit.com/v3/forms/${formId}/subscribe`, {
+    body: JSON.stringify({ api_key: publicApiKey, email }),
+    headers: { 'Content-Type': 'application/json' },
+    method: 'post',
+  });
+}
