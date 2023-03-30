dc0f66e6bcb294ad76303baeb3ea0e36d38fa83e
Glenn Reyes <glenn@glennreyes.com>
2023-03-30 15:41:56 +0200

    Fix typo

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -1,16 +1,12 @@
 'use client';
 
 import type { ReactNode } from 'react';
-import { IntersectionProvider } from '../intersection/InterSectionProvider';
+import { IntersectionProvider } from '../intersection/IntersectionProvider';
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return (
-    <>
-      <IntersectionProvider>{children}</IntersectionProvider>
-    </>
-  );
+  return <IntersectionProvider>{children}</IntersectionProvider>;
 }
