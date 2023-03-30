1038abe4cdf32e3bb2def88f161b319d632d5b2c
Glenn Reyes <glenn@glennreyes.com>
2023-03-30 10:06:03 +0200

    Improve `H1` gradient

--- components/ui/typography/H1.tsx
+++ components/ui/typography/H1.tsx
@@ -5,13 +5,16 @@ type H1Props<TElementType extends ElementType> = ComponentPropsWithoutRef<TEleme
   as?: Extract<TElementType, 'h1' | 'h2' | 'h3' | 'h4'>;
 };
 
-export function H1<TElementType extends ElementType>({ as, className, ...props }: H1Props<TElementType>) {
+export function H1<TElementType extends ElementType>({ as, children, className, ...props }: H1Props<TElementType>) {
   const Component = as ?? 'h1';
   // TODO: Switch to tailwind-merge once it supports Tailwind 3.3
-  const classes = clsx(
-    'text-4xl/tight sm:text-5xl/tight bg-gradient-to-r from-black to-slate-500 bg-clip-text font-extrabold tracking-tighter text-transparent',
-    className,
-  );
+  const classes = clsx('text-4xl/tight sm:text-5xl/tight font-extrabold tracking-tighter', className);
 
-  return <Component className={classes} {...props} />;
+  return (
+    <Component className={classes} {...props}>
+      <span className="inline-block bg-gradient-to-r from-black to-slate-500 bg-clip-text text-transparent">
+        {children}
+      </span>
+    </Component>
+  );
 }
