c4e15ef693ac623a6e30e165b77281ba6b6b7605
Glenn Reyes <glenn@glennreyes.com>
2023-04-02 16:05:56 +0200

    Adjust font in input field

--- components/ui/forms/Input.tsx
+++ components/ui/forms/Input.tsx
@@ -5,7 +5,7 @@ type InputProps = ComponentPropsWithoutRef<'input'>;
 
 export function Input({ className, ...props }: InputProps) {
   const classes = twMerge(
-    'rounded-2xl border border-slate-300 py-3 px-5 text-left text-sm font-medium text-slate-600 transition placeholder:text-slate-300 focus:border-slate-400 focus:text-slate-700 focus:ring-4 focus:ring-teal-100 disabled:bg-slate-50',
+    'rounded-2xl border border-slate-300 py-3 px-5 text-left text-sm font-semibold tracking-tight text-slate-600 transition placeholder:text-slate-300 focus:border-slate-400 focus:text-slate-700 focus:ring-4 focus:ring-teal-100 disabled:bg-slate-50',
     className,
   );
 
