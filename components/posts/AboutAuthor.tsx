6ec1fe43864cafbf35d678db905a9cee36559c11
Glenn Reyes <glenn@glennreyes.com>
2023-04-02 16:04:58 +0200

    Adjust `AboutAuthor` component

--- components/posts/AboutAuthor.tsx
+++ components/posts/AboutAuthor.tsx
@@ -8,14 +8,14 @@ import { H4 } from '../ui/typography/H4';
 export function AboutAuthor() {
   return (
     <Card as="section">
-      <Card.Body>
+      <Card.Body title="About The Author">
         <div className="grid items-center gap-6 sm:flex">
           <Link className="flex-none rounded-full" href="/about">
             <Image alt={name} className="h-28 w-28 rounded-full border-2 border-slate-200" src={photo} />
           </Link>
           <div className="grid gap-4">
             <H4 as="h2">
-              <Link href="/about">About {name}</Link>
+              <Link href="/about">{name}</Link>
             </H4>
             <p className="text-sm text-slate-500">{description}</p>
           </div>
