e3fd5473d63057c243483e5bbc22d840a3d375a5
Glenn Reyes <glenn@glennreyes.com>
2023-04-02 16:05:42 +0200

    Adjust arrow in back link

--- components/posts/BackLink.tsx
+++ components/posts/BackLink.tsx
@@ -1,14 +1,17 @@
-import { ArrowLeftIcon } from '@heroicons/react/24/solid';
+import { ArrowSmallLeftIcon } from '@heroicons/react/20/solid';
 import { Link } from '../ui/link/Link';
 
 export function BackLink() {
   return (
     <Link
       aria-label="Back to posts"
-      className="group inline-flex rounded-full border border-slate-100 p-3 transition hover:border-slate-300 focus-visible:border-slate-300"
+      className="group inline-flex rounded-full border border-slate-100 p-2.5 transition hover:border-slate-300 focus-visible:border-slate-300"
       href="/posts"
     >
-      <ArrowLeftIcon aria-hidden className="h-4 w-4 text-slate-500 hover:text-slate-600 focus-visible:text-slate-600" />
+      <ArrowSmallLeftIcon
+        aria-hidden
+        className="h-6 w-6 text-slate-500 hover:text-slate-600 focus-visible:text-slate-600"
+      />
     </Link>
   );
 }
