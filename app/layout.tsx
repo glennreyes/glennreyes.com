c2c17eb2e1197900f0592a4b26823312b05f2d31
Glenn Reyes <glenn@glennreyes.com>
2023-04-03 11:56:50 +0200

    Add analytics to the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { ReactNode } from 'react';
 import '~/app/globals.css';
 import appleTouchIcon from '~/assets/favicon/apple-touch-icon.png';
@@ -39,6 +40,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Main>{children}</Main>
         </Providers>
         <Footer />
+        <Analytics />
       </Body>
     </Html>
   );
