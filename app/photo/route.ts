493f66b91432adf81ef178e9e6d47b1b8a51a485
Glenn Reyes <glenn@glennreyes.com>
2023-04-03 11:53:25 +0200

    Add a route to serve a photo

---
+++ app/photo/route.ts
@@ -0,0 +1,13 @@
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+export async function GET() {
+  // Read the JPG photo from the assets/images directory
+  const path = join(process.cwd(), 'assets', 'images', 'photo.jpg');
+  const buffer = await readFile(path);
+
+  // Return the photo as a response with the correct content type
+  return new Response(buffer, {
+    headers: { 'Content-Type': 'image/jpeg' },
+  });
+}
