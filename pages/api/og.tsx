8578ccc9025bf5ef9e98bf9248cd95297d0216b4
Glenn Reyes <glenn@glennreyes.com>
2023-04-07 21:39:21 +0200

    Revamp OG image generation

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -9,51 +9,57 @@ export const config = {
   runtime: 'edge',
 };
 
+// eslint-disable-next-line @next/next/no-img-element
+const defaultImageResponse = new ImageResponse(<img alt="" src={`${origin}/og.png`} tw="h-full w-full" />, {
+  height: 1080,
+  width: 1920,
+});
+
 export default async function handler(request: NextRequest) {
   const [interBold, interRegular] = await Promise.all([
     fetch(new URL('../../assets/fonts/Inter-Bold.ttf', import.meta.url)).then((res) => res.arrayBuffer()),
     fetch(new URL('../../assets/fonts/Inter-Regular.ttf', import.meta.url)).then((res) => res.arrayBuffer()),
   ]);
+  const postSlug = request.nextUrl.searchParams.get('post');
 
-  const { searchParams } = request.nextUrl;
-  const id = searchParams.get('post');
-  const post = allPosts.find(({ slug }) => slug === id);
-
-  if (!post) {
-    // eslint-disable-next-line @next/next/no-img-element
-    return new ImageResponse(<img alt="" src={`${origin}/images/og.png`} tw="h-full w-full" />, {
-      height: 1080,
-      width: 1920,
-    });
+  if (!postSlug) {
+    return defaultImageResponse;
   }
 
-  const date = new Date(post.publishedAt);
+  const post = allPosts.find(({ slug }) => slug === postSlug);
 
-  return new ImageResponse(
-    (
-      <div
-        style={{ backgroundImage: `url(${origin}/images/og-post.png)` }}
-        tw="bg-slate-900 flex flex-col justify-center h-full w-full px-48 pt-40 pb-80"
-      >
-        <time tw="text-4xl text-slate-500">{format(date, isThisYear(date) ? 'MMMM d' : 'MMMM d, yyyy')}</time>
-        <h1 tw="text-9xl font-bold tracking-tighter font-bold text-slate-50 pt-2">{post.title}</h1>
-      </div>
-    ),
-    {
-      fonts: [
-        {
-          data: interRegular,
-          name: 'Inter',
-          weight: 400,
-        },
-        {
-          data: interBold,
-          name: 'Inter',
-          weight: 700,
-        },
-      ],
-      height: 1080,
-      width: 1920,
-    },
-  );
+  if (post) {
+    const date = new Date(post.publishedAt);
+    const dateFormatted = format(date, isThisYear(date) ? 'MMMM d' : 'MMMM d, yyyy');
+
+    return new ImageResponse(
+      (
+        <div
+          style={{ backgroundImage: `url(${origin}/images/og-content.png)` }}
+          tw="bg-slate-900 flex flex-col justify-center h-full w-full px-48 pt-40 pb-80"
+        >
+          <time tw="text-4xl text-slate-500">{dateFormatted}</time>
+          <h1 tw="text-9xl font-bold tracking-tighter font-bold text-slate-50 pt-2">{post.title}</h1>
+        </div>
+      ),
+      {
+        fonts: [
+          {
+            data: interRegular,
+            name: 'Inter',
+            weight: 400,
+          },
+          {
+            data: interBold,
+            name: 'Inter',
+            weight: 700,
+          },
+        ],
+        height: 1080,
+        width: 1920,
+      },
+    );
+  }
+
+  return defaultImageResponse;
 }
