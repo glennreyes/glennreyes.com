313671121f7430eb586ceaffb4d78809fdb460af
Glenn Reyes <glenn@glennreyes.com>
2023-04-18 10:35:58 +0200

    Add spin animation to avatar

--- components/avatar/Avatar.tsx
+++ components/avatar/Avatar.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import clsx from 'clsx';
 import Image from 'next/image';
 import type { ComponentPropsWithoutRef } from 'react';
-import { twMerge } from 'tailwind-merge';
 import photo from '~/assets/images/photo.jpg';
 import { name } from '~/lib/constants';
 
@@ -10,17 +10,17 @@ type AvatarSize = 11 | 28 | 40;
 
 const attributes: Record<AvatarSize, Pick<ComponentPropsWithoutRef<typeof Image>, 'className' | 'height' | 'width'>> = {
   11: {
-    className: 'h-11 w-11 border',
+    className: 'h-11 w-11 p-px',
     height: 44,
     width: 44,
   },
   28: {
-    className: 'h-28 w-28 border-2',
+    className: 'h-28 w-28 p-0.5',
     height: 112,
     width: 112,
   },
   40: {
-    className: 'h-40 w-40 border-4',
+    className: 'h-40 w-40 p-1',
     height: 160,
     width: 160,
   },
@@ -36,7 +36,19 @@ interface AvatarProps
 
 export function Avatar({ size = 40, ...props }: AvatarProps) {
   const { className, ...sizeAttributes } = attributes[size];
-  const classes = twMerge(className, 'border-slate-200 dark:border-slate-900 rounded-full');
+  const classes = clsx('relative rounded-full', className);
 
-  return <Image alt={name} className={classes} placeholder="blur" src={photo} {...sizeAttributes} {...props} />;
+  return (
+    <div className={classes}>
+      <div className="dark:from-slate-transparent dark:to-slate-transparent absolute inset-0 rounded-full bg-gradient-to-r from-transparent via-slate-200 to-transparent motion-safe:animate-spin motion-safe:[animation-duration:10s] dark:via-slate-800/50" />
+      <Image
+        alt={name}
+        className="relative rounded-full"
+        placeholder="blur"
+        src={photo}
+        {...sizeAttributes}
+        {...props}
+      />
+    </div>
+  );
 }
