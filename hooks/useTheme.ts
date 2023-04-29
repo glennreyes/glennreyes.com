0c7efe37afe3a1328d178a2146121335357aa8b6
Glenn Reyes <glenn@glennreyes.com>
2023-04-30 00:02:30 +0200

    Toggle meta theme color based on theme

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,10 +1,17 @@
 import { useTheme as useThemeBase } from 'next-themes';
+import { useEffect } from 'react';
 import { parseResolvedTheme, parseTheme } from '~/lib/theme';
 
 export function useTheme() {
   const { theme: themeBase, setTheme, resolvedTheme: resolvedThemeBase } = useThemeBase();
   const theme = parseTheme(themeBase);
   const resolvedTheme = parseResolvedTheme(resolvedThemeBase);
+
+  useEffect(() => {
+    document.head
+      .querySelector('meta[name="theme-color"]')
+      ?.setAttribute('content', resolvedTheme === 'dark' ? '#020617' : '#ffffff');
+  }, [resolvedTheme]);
 
   return { resolvedTheme, setTheme, theme };
 }
