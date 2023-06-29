7985377a2a493e55e3e54fb8631daf608d7b4945
Glenn Reyes <glenn@glennreyes.com>
2023-06-29 17:15:48 +0200

    Use correct import for PrismaClient

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
-import { prisma } from '@/lib/prisma';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 (async () => {
   console.info('Seeding database...', prisma);
