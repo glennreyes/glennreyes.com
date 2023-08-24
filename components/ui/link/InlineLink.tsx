8c4b32daecf7428ee936afd7518b8cae7e6bcfab
Glenn Reyes <glenn@glennreyes.com>
2023-08-24 22:34:00 +0200

    Change underline thickness

--- components/ui/link/InlineLink.tsx
+++ components/ui/link/InlineLink.tsx
@@ -6,7 +6,7 @@ type InlineLinkProps = ComponentPropsWithoutRef<typeof Link>;
 
 export function InlineLink({ className, ...props }: InlineLinkProps) {
   const classes = twMerge(
-    'text-slate-800 underline decoration-teal-200 decoration-2 underline-offset-4 transition hover:text-slate-950 hover:decoration-teal-400 focus-visible:text-slate-600 focus-visible:no-underline dark:text-slate-200 dark:decoration-teal-700 hover:dark:text-slate-50 dark:hover:decoration-teal-500',
+    'text-slate-800 underline decoration-teal-200 decoration-1 underline-offset-4 transition hover:text-slate-950 hover:decoration-teal-400 focus-visible:text-slate-600 focus-visible:no-underline dark:text-slate-200 dark:decoration-teal-700 hover:dark:text-slate-50 dark:hover:decoration-teal-500',
     className,
   );
 
