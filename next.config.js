069dfde965099af17c53e062dc4b8d72c2801dd1
Glenn Reyes <glenn@glennreyes.com>
2023-10-26 19:18:11 +0100

    Remove `appDir` config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     mdxRs: true,
   },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
