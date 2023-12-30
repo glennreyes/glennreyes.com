d842e4e9f7b137da01b145c735fa2aec7b57f576
Glenn Reyes <glenn@glennreyes.com>
2023-12-30 09:59:49 +0100

    Disable linting errors in `contentlayer.config.ts`

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -61,20 +61,25 @@ export default makeSource({
           properties: { className: [classes.autoLink] },
         } satisfies Parameters<typeof rehypeAutolinkHeadings>[0],
       ],
+      // eslint-disable-next-line unicorn/consistent-function-scoping
       () => (tree) => {
         // Extract raw code from `code` element nested inside `pre` tag and store value in `pre` node.
         // This need to be before `rehypePrettyCode` so that we get the raw value.
         visit(tree, (node) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           if (!(node?.type === 'element' && node?.tagName === 'pre')) {
             return;
           }
 
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
           const [element] = node.children;
 
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           if (element.tagName !== 'code') {
             return;
           }
 
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
           node.raw = element.children?.[0].value;
         });
       },
@@ -99,19 +104,25 @@ export default makeSource({
           theme: 'nord',
         } satisfies Parameters<typeof rehypePrettyCode>[0],
       ],
+      // eslint-disable-next-line unicorn/consistent-function-scoping
       () => (tree) => {
         // Pass raw value found in `pre` node and store to div[data-rehype-pretty-code-fragment]
         // This is after `rehypePrettyCode` so the stored raw code can be moved to the target div.
         visit(tree, (node) => {
           if (
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             !(node?.type === 'element' && node?.tagName === 'div') ||
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             !('data-rehype-pretty-code-fragment' in node.properties)
           ) {
             return;
           }
 
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           for (const child of node.children) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             if (child.tagName === 'pre') {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
               node.properties.raw = node.raw;
             }
           }
