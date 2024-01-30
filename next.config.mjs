418c8a3dd71efb6e0199c9af41b3b50fbfa4d4e7
Glenn Reyes <glenn@glennreyes.com>
2024-01-30 13:56:50 +0100

    Add path to favorite tools

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: () => [
+    {
+      destination:
+        'https://www.notion.so/glennreyes/Tools-6df9f0a402784ef7a093452d464e793e',
+      source: '/tools',
+    },
+  ],
 };
 
 export default nextConfig;
