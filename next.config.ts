620f66b3de4d84c0fa64c1860542eae063b43255
Glenn Reyes <glenn@glennreyes.com>
2025-09-08 13:10:36 +0200

    Omit defaults

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  reactStrictMode: true,
   redirects: () =>
     Promise.resolve([
       {
