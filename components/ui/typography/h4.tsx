03a00abd6837ba5ff3411299967fdaf7ef3bf6b7
Glenn Reyes <glenn@glennreyes.com>
2025-09-08 13:21:28 +0200

    Tweak comments section

--- components/ui/typography/h4.tsx
+++ components/ui/typography/h4.tsx
@@ -9,7 +9,7 @@ interface H4Props extends ComponentPropsWithoutRef<'h4'> {
 
 export const H4 = ({ asChild, className, ...props }: H4Props) => {
   const classes = twMerge(
-    'text-slate-700 dark:text-slate-300 font-medium',
+    'text-slate-700 dark:text-slate-300 font-medium text-base',
     className,
   );
   const Component = asChild ? Slot : 'h4';
