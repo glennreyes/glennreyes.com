766a2f5a2aa6e9887d903e96a609bda2cccbf597
Glenn Reyes <glenn@glennreyes.com>
2025-09-10 15:17:20 +0200

    OG tweaks

--- app/posts/[slug]/opengraph-image.tsx
+++ app/posts/[slug]/opengraph-image.tsx
@@ -16,24 +16,71 @@ interface OpenGraphImageConfig {
 }
 
 const OpengraphImage = async ({ params }: OpenGraphImageConfig) => {
-  const res = await fetch(`${origin}/posts/${params.slug}/meta`);
-  const { publishedAt, title } = z
-    .object({ publishedAt: z.string(), title: z.string() })
-    .parse(await res.json());
-  const geistSans700 = fetch(
-    new URL(
-      '../../../node_modules/@fontsource/geist/files/geist-latin-700-normal.woff',
-      import.meta.url,
-    ),
-  ).then((res) => res.arrayBuffer());
-  const geistSans400 = fetch(
-    new URL(
-      '../../../node_modules/@fontsource/geist/files/geist-latin-400-normal.woff',
-      import.meta.url,
-    ),
-  ).then((res) => res.arrayBuffer());
+  try {
+    const res = await fetch(`${origin}/posts/${params.slug}/meta`);
 
-  if (!title) {
+    if (!res.ok) {
+      throw new Error(`Failed to fetch meta data: ${res.status}`);
+    }
+
+    const { publishedAt, title } = z
+      .object({ publishedAt: z.string(), title: z.string() })
+      .parse(await res.json());
+    const geistSans500Response = await fetch(
+      new URL(
+        '../../../node_modules/@fontsource/geist/files/geist-latin-500-normal.woff2',
+        import.meta.url,
+      ),
+    );
+    const geistSans500 = await geistSans500Response.arrayBuffer();
+    const geistSans400Response = await fetch(
+      new URL(
+        '../../../node_modules/@fontsource/geist/files/geist-latin-400-normal.woff2',
+        import.meta.url,
+      ),
+    );
+    const geistSans400 = await geistSans400Response.arrayBuffer();
+
+    if (!title) {
+      return new ImageResponse(
+        (
+          <div
+            style={{ backgroundImage: `url(${origin}/opengraph-image.png)` }}
+            tw="h-full w-full"
+          />
+        ),
+        size,
+      );
+    }
+
+    return new ImageResponse(
+      (
+        <div
+          style={{ backgroundImage: `url(${origin}/images/og-content.png)` }}
+          tw="bg-slate-900 flex flex-col h-full w-full px-48 pt-40 pb-80"
+        >
+          <time tw="text-8xl text-slate-500">{publishedAt}</time>
+          <h1 tw="text-8xl font-medium text-slate-50 pt-6">{title}</h1>
+        </div>
+      ),
+      {
+        fonts: [
+          {
+            data: geistSans400,
+            name: 'Inter',
+            weight: 400,
+          },
+          {
+            data: geistSans500,
+            name: 'Inter',
+            weight: 500,
+          },
+        ],
+        ...size,
+      },
+    );
+  } catch {
+    // Fallback to default OpenGraph image
     return new ImageResponse(
       (
         <div
@@ -44,33 +91,6 @@ const OpengraphImage = async ({ params }: OpenGraphImageConfig) => {
       size,
     );
   }
-
-  return new ImageResponse(
-    (
-      <div
-        style={{ backgroundImage: `url(${origin}/images/og-content.png)` }}
-        tw="bg-slate-900 flex flex-col h-full w-full px-48 pt-40 pb-80"
-      >
-        <time tw="text-8xl text-slate-500">{publishedAt}</time>
-        <h1 tw="text-8xl font-medium text-slate-50 pt-6">{title}</h1>
-      </div>
-    ),
-    {
-      fonts: [
-        {
-          data: await geistSans400,
-          name: 'Inter',
-          weight: 400,
-        },
-        {
-          data: await geistSans700,
-          name: 'Inter',
-          weight: 700,
-        },
-      ],
-      ...size,
-    },
-  );
 };
 
 export default OpengraphImage;
