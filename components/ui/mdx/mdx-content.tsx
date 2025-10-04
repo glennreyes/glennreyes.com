269ef68dc6eccdf701e45e6bb2a0a13a1844ee0d
Glenn Reyes <glenn@glennreyes.com>
2025-10-04 19:55:40 +0200

    Simplify MDXContent component by removing unused options merging
    Remove unnecessary options parameter and cloneDefaultOptions function since options are never overridden in practice. Keep components override functionality which is actively used.

--- components/ui/mdx/mdx-content.tsx
+++ components/ui/mdx/mdx-content.tsx
@@ -7,46 +7,16 @@ import { mdxRemoteOptions } from '@/lib/mdx/read-mdx-file';
 
 type MDXContentProps = Omit<MDXRemoteProps, 'components' | 'options'> & {
   components?: MDXRemoteProps['components'];
-  options?: MDXRemoteProps['options'];
 };
-
-const cloneDefaultOptions = () => ({
-  ...mdxRemoteOptions,
-  mdxOptions: {
-    ...mdxRemoteOptions.mdxOptions,
-    rehypePlugins: [...(mdxRemoteOptions.mdxOptions?.rehypePlugins ?? [])],
-    remarkPlugins: [...(mdxRemoteOptions.mdxOptions?.remarkPlugins ?? [])],
-  },
-});
 
 export function MDXContent({
   components: overrides,
-  options,
   ...props
 }: MDXContentProps) {
-  const mergedComponents = { ...components, ...overrides };
-  const defaultOptions = cloneDefaultOptions();
-  const mergedOptions = options
-    ? {
-        ...defaultOptions,
-        ...options,
-        mdxOptions: {
-          ...defaultOptions?.mdxOptions,
-          ...options?.mdxOptions,
-          rehypePlugins:
-            options?.mdxOptions?.rehypePlugins ??
-            defaultOptions?.mdxOptions?.rehypePlugins,
-          remarkPlugins:
-            options?.mdxOptions?.remarkPlugins ??
-            defaultOptions?.mdxOptions?.remarkPlugins,
-        },
-      }
-    : defaultOptions;
-
   return (
     <MDXRemote
-      components={mergedComponents}
-      options={mergedOptions}
+      components={{ ...components, ...overrides }}
+      options={mdxRemoteOptions}
       {...props}
     />
   );
