c7b6fcbe61f105e4a0d2bd8057545c6ce5624941
Glenn Reyes <glenn@glennreyes.com>
2025-10-09 11:26:19 +0200

    Reorder post footer sections to place newsletter before comments

--- components/posts/post-footer.tsx
+++ components/posts/post-footer.tsx
@@ -6,11 +6,11 @@ export function PostFooter() {
   return (
     <>
       <AboutAuthor />
-      <Comments />
       <Newsletter title="Subscribe to my Newsletter">
         Get subscribed to stay in the loop and receive occasional news and
         updates directly to your inbox. No spam, unsubscribe at any time.
       </Newsletter>
+      <Comments />
     </>
   );
 }
